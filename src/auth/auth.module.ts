0721ee88ec69864c41716e505437643c07c96179
André Parada <andregparada@gmail.com>
2025-09-02 14:36:45 -0300

    alterando keys

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,53 @@
 import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
-import { ConfigService } from '@nestjs/config'
-import { Env } from 'src/env'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+// @Module({
+//   imports: [
+//     PassportModule,
+//     JwtModule.registerAsync({
+//       inject: [ConfigService],
+//       global: true,
+//       useFactory(config: ConfigService<Env, true>) {
+//         const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+//         const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+
+//         return {
+//           signOptions: { algorithm: 'RS256' },
+//           privateKey: Buffer.from(privateKey, 'base64'),
+//           publicKey: Buffer.from(publicKey, 'base64'),
+//         }
+//       },
+//     }),
+//   ],
+// })
+// export class AuthModule {}
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
-      global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+      inject: [],
+      useFactory() {
+        const privateKey = readFileSync(
+          join(__dirname, '../../keys/private_key.pem'),
+          'utf8',
+        )
+        const publicKey = readFileSync(
+          join(__dirname, '../../keys/public_key.pem'),
+          'utf8',
+        )
 
         return {
           signOptions: { algorithm: 'RS256' },
-          privateKey: Buffer.from(privateKey, 'base64'),
-          publicKey: Buffer.from(publicKey, 'base64'),
+          privateKey,
+          publicKey,
         }
       },
     }),
   ],
+  exports: [JwtModule],
 })
 export class AuthModule {}
