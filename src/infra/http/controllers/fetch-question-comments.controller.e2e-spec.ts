d10c904cb7f3e5c0690c049f1fad955348d81aa7
André Parada <andregparada@gmail.com>
2025-12-05 10:55:18 -0300

    Controller comentário com autor

--- src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
@@ -31,8 +31,8 @@ describe('Fetch question comments (E2E)', () => {
     await app.init()
   })
 
-  test('[GET] /questions/:questionId/comment', async () => {
-    const user = await studentFactory.makePrismaStudent()
+  test('[GET] /questions/:questionId/comments', async () => {
+    const user = await studentFactory.makePrismaStudent({ name: 'John Doe' })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -63,8 +63,14 @@ describe('Fetch question comments (E2E)', () => {
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
       comment: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 01' }),
-        expect.objectContaining({ content: 'Comment 02' }),
+        expect.objectContaining({
+          content: 'Comment 01',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Comment 02',
+          authorName: 'John Doe',
+        }),
       ]),
     })
   })
